42a95a36b6abcb8ed19810fa5518a7ffabf91d76
yohanht23 <yohanht23@gmail.com>
2016-11-28 09:09:46 -0400

    El Footer mejorado

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<div class="footer" id="footer">
+	<div class="container">
+		<ul class="float-xs-left navbar-link footer-nav">
+			<li>
+				<a href="#">Inicio</a>
+				<a href="#">Acerca de</a>
+				<a href="#">Terminos y Condiciones</a>
+				<a href="#">Politicas de privacidad</a>
+				<a href="#">Contactanos</a>
+				<a href="#">FAQ</a>
+			</li>
+		</ul>
+		<ul class="float-xs-right navbar-link footer-nav">
+			<li>
+				&copy; 2016 TEAM FLYBOX
+			</li>
+		</ul>
+	</div>
+</div>
