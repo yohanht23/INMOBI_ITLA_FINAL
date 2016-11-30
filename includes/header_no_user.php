f84201b625d3ffdbc9f08d16fdaf54707b668dcc
Valdali18 <valdalilora@gmail.com>
2016-11-29 21:10:02 -0800

    Head_no_user

---
+++ includes/header_no_user.php
@@ -0,0 +1,32 @@
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-3">
+				<a href="./" class="header-brand">
+					<img src="logo.png" alt="">
+					INMOBI
+					<span class="light">ITLA</span>
+				</a>
+
+			</div>
+			<div class="col-xs-9">
+				<div class="float-xs-right">
+					<div class="header-menu">
+						<div class="header-menu-item">
+							<a href="modo_mapa.php"><i class="fa fa-map-marker"></i> Vista Mapa</a>
+						</div>
+						<div class="header-menu-item">
+							<a href="./?query=Login"><i class="fa fa-sign-in"></i> Iniciar sesion</a>
+						</div>
+						<div class="header-menu-item">
+							<a href="./?query=Signup"><i class="fa fa-user-plus"></i> Registrarme</a>
+						</div>
+						<div class="header-menu-item">
+							<a href="./?query=Publicar" class="btn btn-danger"><i class="fa fa-plus"></i> PUBLICAR ANUNCIO</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
